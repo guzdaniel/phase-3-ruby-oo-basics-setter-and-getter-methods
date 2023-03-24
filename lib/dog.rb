ab660fd013484d5aa72d5da7da9c0bc8e94ca61f
Daniel Guzman <guzdan@live.com>
2023-03-24 16:30:24 -0500

    commit tree 7341a89f12e62b151c7704d4aaf7ecfaafeec4b3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+       @name 
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+d = Dog.new
+puts d.name
+d.name = 'Billy'
+
+puts d.name
+
+d.breed = 'Corgi'
+
+puts d.breed
